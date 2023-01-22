b574c7b14e2654118048ab294ea7b9662ef78d56
Kanks <91976999+KanksAlex@users.noreply.github.com>
2023-01-22 15:48:26 +0300

    drawer

--- client/src/layout/Drawer/index.js
+++ client/src/layout/Drawer/index.js
@@ -1,3 +1,14 @@
+import {useMemo} from 'react';
+import DrawerContent from './DrawerContent';
+import {Box, Drawer,} from '@mui/material';
+
 const MainDrawer =() => {
-    
+   const drawerContent =useMemo(() => <DrawerContent/> ,[]);
+
+   return(
+    <Box component="nav"  sx={{ flexShrink: {md:0}, zIndex: 1300}} aria-label="mailbox folders">
+        
+    </Box>
+   );
 }; 
+export default MainDrawer;
