a948b9377c39c673439fdff4e90881695127ec0c
TONNY BAWEMBYE <tonnybaw75@gmail.com>
2023-01-22 19:31:27 +0300

    mini fix

--- client/src/layout/MainLayout/Drawer/index.js
+++ client/src/layout/MainLayout/Drawer/index.js
@@ -1,17 +1,45 @@
-import {useMemo} from 'react';
-import DrawerContent from './DrawerContent';
 import { Box } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { useMemo } from 'react';
+import DrawerContent from './DrawerContent';
 import MiniDrawerStyled from './MiniDrawerStyled';
 
-const MainDrawer =({ open, window }) => {
+const MainDrawer =({ open, handleDrawerToggle, window }) => {
+    const theme = useTheme();
+    const matchDownMD = useMediaQuery(theme.breakpoints.down('lg'));
+
+    // responsive drawer container
+    const container = window !== undefined ? () => window().document.body : undefined;
+
    const drawerContent =useMemo(() => <DrawerContent/> ,[]);
 
    return(
-    <Box component="nav"  sx={{ flexShrink: {md:0}, zIndex: 1300}} aria-label="mailbox folders">
-        <h1>How djkksd</h1>
-        <MiniDrawerStyled variant="permanent" open={open}>
-            {drawerContent}
-        </MiniDrawerStyled>
+    <Box component="nav" sx={{ flexShrink: { md: 0 }, zIndex: 1300 }} aria-label="mailbox folders">
+        {!matchDownMD ? (
+                <MiniDrawerStyled variant="permanent" open={open}>
+                    {drawerContent}
+                </MiniDrawerStyled>
+            ) : (
+                <Drawer
+                    container={container}
+                    variant="temporary"
+                    open={open}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{ keepMounted: true }}
+                    sx={{
+                        display: { xs: 'block', lg: 'none' },
+                        '& .MuiDrawer-paper': {
+                            boxSizing: 'border-box',
+                            width: drawerWidth,
+                            borderRight: `1px solid ${theme.palette.divider}`,
+                            backgroundImage: 'none',
+                            boxShadow: 'inherit'
+                        }
+                    }}
+                >
+                    {open && drawerContent}
+                </Drawer>
+            )}
     </Box>
    );
 }; 
