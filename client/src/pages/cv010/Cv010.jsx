62875a89553e39559a4f19c9e1490ac31bdec419
Kanks <91976999+KanksAlex@users.noreply.github.com>
2023-02-09 13:11:05 +0300

    sc

--- client/src/pages/cv010/Cv010.jsx
+++ client/src/pages/cv010/Cv010.jsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import "./matrix.css";
 import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import { DataGridPro } from '@mui/x-data-grid-pro';
@@ -25,12 +29,26 @@ export default function DataGridProDemo() {
 
   ];
 
+  const results = [
+    { label: 'Positive' },
+    { label: 'Negative' }
+  ];
+
+
+  const [sex, setSex] = React.useState('');
+
+  const handleChange = (event) => {
+    setSex(event.target.value);
+  };
+
+
+
   return (
     <div style={{ height: 700, width: '100%' }}>
 
       <div className="featured">
         <div className="featuredItem">
-
+<div style={{marginBottom:20,}}>
           <TextField
             label="PID"
             id="outlined-start-adornment"
@@ -42,7 +60,7 @@ export default function DataGridProDemo() {
 
           <TextField
             id="outlined-number outlined-start-adornment"
-            label="Age"
+            label=" PCR result (Ct value ORF 1a/b gene)2"
             sx={{ m: 1, width: '25ch' }}
             type="number"
             InputLabelProps={{
@@ -50,12 +68,78 @@ export default function DataGridProDemo() {
             }}
           />
 
+          <TextField
+            id="outlined-number outlined-start-adornment"
+            label="  PCR result (E-GENE)2"
+            sx={{ m: 1, width: '25ch' }}
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+
+          <TextField
+            id="outlined-number outlined-start-adornment"
+            label=" PCR result (internal CTRL)"
+            sx={{ m: 1, width: '25ch' }}
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+  </div>
+          {/* 
+          <Box sx={{ m: 1, width: '25ch' }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Sex</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={sex}
+                label="Sex"
+                onChange={handleChange}
+              >
+                <MenuItem value={1}>Male</MenuItem>
+                <MenuItem value={2}>Female</MenuItem>
+              </Select>
+            </FormControl>
+          </Box> */}
+
+
           <Autocomplete
             disablePortal
-            id="outlined-start-adornment"
+            id="outlined-number outlined-start-adornment"
             options={top100Films}
             sx={{ m: 1, width: '25ch' }}
             renderInput={(params) => <TextField {...params} label="facility" />}
+          />
+
+
+
+          <Autocomplete
+            disablePortal
+            id="outlined-number outlined-start-adornment"
+            options={results}
+            sx={{ m: 1, width: '25ch' }}
+            renderInput={(params) => <TextField {...params} label="PCR result (Pos/Neg)" />}
+          />
+
+          <Autocomplete
+            disablePortal
+            id="outlined-number outlined-start-adornment"
+            options={results}
+            sx={{ m: 1, width: '25ch' }}
+            renderInput={(params) => <TextField {...params} label="POC result (Pos/Neg)" />}
+          />
+
+          <TextField
+            id="outlined-number outlined-start-adornment"
+            label="POC result (Ct Value)"
+            sx={{ m: 1, width: '25ch' }}
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
           />
 
 
